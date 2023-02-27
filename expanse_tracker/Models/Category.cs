d36777fb68bd4137e0feb1db092034bfe6d71f1f
Trong Dat <datnguyentrong98@gmail.com>
2023-02-27 13:12:13 +1000

    adding category controller and its views

--- expanse_tracker/Models/Category.cs
+++ expanse_tracker/Models/Category.cs
@@ -19,6 +19,13 @@ namespace expanse_tracker.Models
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
 
+        [NotMapped]
+        public string? TitleWithIcon {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
 
